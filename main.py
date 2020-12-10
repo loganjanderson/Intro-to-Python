46506534759dbbe128c1bfec4f1d58605e06dd1a
Logan Anderson <loganjohnanderson@gmail.com>
2020-12-10 03:45:57 +0000

    created first varirables

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-def manual_icrementing_matrix(n):
-    # matrix n x n
-    matrix = [ [ None for y in range(n)] for x in range(n)]
-    
-
-    counter = 0 
-
-    for idx, el in enumerate(matrix):
-
-        for nested_idx, nested_el in enumerate(el):
-            matrix[idx][nested_idx] = counter + nested_idx
-
-        counter += 1
+name = 'Logan'
+post_count = 42
 
 
-    return matrix
 
-print(manual_icrementing_matrix(10))
+
+
+
+print(name)
+print(post_count)
