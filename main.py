0389e0f2aaaedcd6704eed1d58e05d19a8670fc3
Logan Anderson <loganjohnanderson@gmail.com>
2020-12-15 03:44:31 +0000

    Added first stings

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-meal_compleated = True
-total = 100
-tip = total * 1/5
-total = total + tip
-receipt = "Your total is " + str(total)
-print(receipt)
+sentence = 'The quick brown fox jumped over the lazy dog'
+
+sentence_two = 'That is my dog\'s bowl'
+
+sentence_three = "That is my dog's bowl"
