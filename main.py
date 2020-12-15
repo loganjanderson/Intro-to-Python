b739643cda317c71412874f958ae4e1f74d6c465
Logan Anderson <loganjohnanderson@gmail.com>
2020-12-15 04:19:11 +0000

    Created first HereDoc

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-#The quick brown fox jumped
 
 
-starter_sentence = 'The quick brown fox jumped'
-new_sentence = 'Thy' + starter_sentence[3:]
-second_new_sentence = starter_sentence[0:10] + 'yellow' + starter_sentence[15:]
+content = """
+Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
 
-print(second_new_sentence)
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
 
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+""".strip()
+
+print(content)
