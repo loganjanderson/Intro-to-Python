c0e712949f2107efeabcc0be63f8d048efda704f
Logan Anderson <loganjohnanderson@gmail.com>
2021-01-06 16:39:35 +0000

    Exploring decimal vs. float numbers

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-url = 'https://google.com'
+from decimal import Decimal 
 
-# print(url.strip('https://'))
+# product_cost = 88.40
+# commission_rate = 0.08
+# qty = 450
 
-url = url.lstrip('https://')
-url = url.rstrip('.com')
-url = url.capitalize()
+# product_cost += (commission_rate * product_cost)
+# print(product_cost * qty) #42962.4
 
-print(url)
+product_cost = Decimal(88.40)
+commission_rate = Decimal(0.08)
+qty = 450
+
+product_cost += (commission_rate * product_cost)
+print(product_cost * qty)
