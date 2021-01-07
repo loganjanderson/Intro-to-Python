165ee29972dbc974a36bb35c78b6e177a39bf8be
Logan Anderson <loganjohnanderson@gmail.com>
2021-01-07 20:34:29 +0000

    Learning how to change lists with remove,pop,an del

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import math
+users = ['a', 'b', 'c', 'd']
 
-loss = -20.25
-product_cost = 89.99
 
-print(abs(loss))
-print(math.floor(product_cost))
-print(math.ceil(product_cost))
-print(abs(math.floor(loss)))
-print(round(product_cost))
-print(math.sqrt(product_cost))
-print(math.pow(5, 2))
+print(users)
+
+users.remove('b')
+
+print(users)
+
+popped_user = users.pop()
+
+print(popped_user)
+print(users)
+
+del users[0]
+
+print(users)
 
