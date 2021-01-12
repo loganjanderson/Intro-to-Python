03520c0265a93956d41351b43edf6ca5d98fceab
Logan Anderson <loganjohnanderson@gmail.com>
2021-01-12 17:11:48 +0000

    Created first dictionary

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-import math
 
-sale_prices = [
-  100,
-  83,
-  220,
-  40,
-  100,
-  400,
-  10,
-  1,
-  3
-]
+players = {
+    "ss": "Correa",
+    "2b": "Altuve",
+    "3b": "Bregman",
+    "DH": "Gattis",
+    "OF": "Springer",
+}
 
-sorted_list = sorted(sale_prices)
-num_of_sales = len(sorted_list)
-first_sales_items = sorted_list[:math.floor(num_of_sales/2)]
-last_sales_items = sorted_list[-(math.floor(num_of_sales/2)):]
-median = sorted_list[math.floor(num_of_sales/2):(math.floor(num_of_sales/2)+1)]
+second_base = players['OF']
 
-print(sorted_list)
-print(num_of_sales) 
-print(first_sales_items)
-print(last_sales_items)
-print(median)
+print(players)
+
+print(second_base)
